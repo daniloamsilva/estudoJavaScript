0a0fe962cd85947d3f347288891c6b71aa7d6d0c
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2019-03-17 19:01:21 -0300

    Add files via upload

---
+++ Desafio 3 - Vogal ou consoante.js
@@ -0,0 +1,15 @@
+/*
+Teste para descobrir se uma
+letra é vogal ou consoante.
+*/
+
+let letra = 'a';
+
+switch (letra){
+	case 'a': console.log('Vogal'); break;
+	case 'e': console.log('Vogal'); break;
+	case 'i': console.log('Vogal'); break;
+	case 'o': console.log('Vogal'); break;
+	case 'u': console.log('Vogal'); break;
+	default: console.log('Consoante'); break;
+}
