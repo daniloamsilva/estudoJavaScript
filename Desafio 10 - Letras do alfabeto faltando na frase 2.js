a7f53635c02f001922ae4db18616c77e1fabbf16
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2019-03-21 19:27:55 -0300

    Update Desafio 10 - Letras do alfabeto faltando na frase 2.js

--- Desafio 10 - Letras do alfabeto faltando na frase 2.js
+++ Desafio 10 - Letras do alfabeto faltando na frase 2.js
@@ -14,7 +14,7 @@ function missingLetters(string){
 
 	// Array com todas as letras do alfabeto
 	let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
-					'n','o','p','q','r','s','t','u','v','w','x','y','z'];
+			'n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 	// Array para guardar as letras não utilzadas
 	let noUsedLetters = [];
